5aa93b1e6c9df66a1bccded33432fde7d45630e5
Murilo Venturoso <muriloventuroso@gmail.com>
2018-08-02 09:22:41 -0300

    Generate PDF

---
+++ meetings/templatetags/pdf.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def next(some_list, current_index):
+    """
+    Returns the next element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) + 1]
+    except Exception:
+        return ''
+
+
+@register.filter
+def previous(some_list, current_index):
+    """
+    Returns the previous element of the list using the current index if it exists.
+    Otherwise returns an empty string.
+    """
+    try:
+        return some_list[int(current_index) - 1]
+    except Exception:
+        return ''
