df3ce4a143c25a21136647abac9bd944d81e347b
Murilo Venturoso <muriloventuroso@gmail.com>
2019-02-18 16:28:18 -0300

    Update

--- sgc/helpers.py
+++ sgc/helpers.py
@@ -7,7 +7,7 @@ def get_mongo_url():
         url = os.environ['MONGODB_URI']
         m = re.match(r"^mongodb:\/\/(?:(?:(\w+)?:(\w+)?@)|:?@?)((?:[\w.-])+)(?::(\d+))?(?:\/([\w-]+))?(?:\?([\w-]+=[\w-]+(?:&[\w-]+=[\w-]+)*)?)?$", url)
         if m:
-            return {'USERNAME': m.group(1), 'PASSWORD': m.group(2), 'HOST': m.group(3), 'PORT': int(m.group(4)), 'NAME': m.group(5)}
+            return {'USER': m.group(1), 'PASSWORD': m.group(2), 'HOST': m.group(3), 'PORT': int(m.group(4)), 'NAME': m.group(5)}
     return {}
 
 
